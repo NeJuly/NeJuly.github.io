a2c315c086b58f853aecb92314689029d29febcc
Junely <ne_july@163.com>
2020-06-19 15:34:43 +0800

    add mysql notes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,21 @@
 module.exports = {
     theme: '@vuepress/theme-blog',
-    title: 'NeJuly 个人博客',
+    title: 'NEJULY',
     description: 'Take a daily note',
     head: [
-        ['link', {rel: 'shortcut icon', href: '../../img/logo.ico'}]
+        ['link', {rel: 'shortcut icon', href: '/assets/img/logo.ico'}]
     ], // 引入标签页小图标，但设置额没用，望告知
     themeConfig: {
         nav: [
             // {text: '主页', link: '/'},
-            {text: 'Charles', link: '/charles/'},
-            {text: 'Docker', link: '/docker/'},
-            {text: 'Idea', link: '/idea/'},
             {text: 'Java', link: '/java/'},
             {text: 'Linux', link: '/linux/'},
             {text: 'Mysql', link: '/mysql/'},
+            {text: 'Docker', link: '/docker/'},
             {text: 'Php', link: '/php/'},
+            {text: 'Idea', link: '/idea/'},
             {text: 'Vultr', link: '/vultr/'},
+            {text: 'Charles', link: '/charles/'},
             {text: 'Wechat', link: '/wechat/'},
             {text: '杂记', link: '/thing/'},
             {
@@ -23,19 +23,57 @@ module.exports = {
                 link: '/tag/',
             },
         ],
-        directories:[
+        directories: [
             {
-                id: 'charles', // Unique id for current classifier
-                dirname: 'charles', // Matched directory name
-                itemLayout: 'charles', // Layout for matched pages.
-                itemPermalink: '/charles/:year/:month/:day/:slug', // Permalink for matched pages.
+                id: 'java', // Unique id for current classifier
+                dirname: 'java', // Matched directory name
+                itemPermalink: '/java/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
+            {
+                id: 'linux', // Unique id for current classifier
+                dirname: 'linux', // Matched directory name
+                itemPermalink: '/linux/:year/:month/:day/:slug', // Permalink for matched pages.
             },
             {
                 id: 'mysql', // Unique id for current classifier
                 dirname: 'mysql', // Matched directory name
-                itemLayout: 'mysql', // Layout for matched pages.
                 itemPermalink: '/mysql/:year/:month/:day/:slug', // Permalink for matched pages.
-            }
+            },
+            {
+                id: 'docker', // Unique id for current classifier
+                dirname: 'docker', // Matched directory name
+                itemPermalink: '/docker/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
+            {
+                id: 'php', // Unique id for current classifier
+                dirname: 'php', // Matched directory name
+                itemPermalink: '/php/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
+            {
+                id: 'idea', // Unique id for current classifier
+                dirname: 'idea', // Matched directory name
+                itemPermalink: '/idea/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
+            {
+                id: 'vultr', // Unique id for current classifier
+                dirname: 'vultr', // Matched directory name
+                itemPermalink: '/vultr/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
+            {
+                id: 'charles', // Unique id for current classifier
+                dirname: 'charles', // Matched directory name
+                itemPermalink: '/charles/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
+            {
+                id: 'wechat', // Unique id for current classifier
+                dirname: 'wechat', // Matched directory name
+                itemPermalink: '/wechat/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
+            {
+                id: 'thing', // Unique id for current classifier
+                dirname: 'thing', // Matched directory name
+                itemPermalink: '/thing/:year/:month/:day/:slug', // Permalink for matched pages.
+            },
         ],
         footer: {
             contact: [
@@ -59,11 +97,11 @@ module.exports = {
                 },
             ],
         },
-        globalPagination : {
-            prevText:'上一页', // Text for previous links.
-            nextText:'下一页', // Text for next links.
-            lengthPerPage:'5', // Maximum number of posts per page.
-            layout:'Pagination', // Layout for pagination page
+        globalPagination: {
+            prevText: '上一页', // Text for previous links.
+            nextText: '下一页', // Text for next links.
+            lengthPerPage: '5', // Maximum number of posts per page.
+            layout: 'Pagination', // Layout for pagination page
         }
     },
 };
---
+++ docs/mysql/2019-10-24-select-date.md
@@ -0,0 +1,76 @@
+---
+title: 查询当天、本周、本月、本季度、本年的数据
+date: 2020-04-17
+author: NeJuly
+tags: 
+  - Mysql
+location: 重庆  
+---
+- 今天
+```mysql
+SELECT * FROM 表名 WHERE TO_DAYS(时间字段名) = TO_DAYS(NOW());
+```
+- 昨天
+```mysql
+SELECT * FROM 表名 WHERE TO_DAYS(NOW()) - TO_DAYS(时间字段名) = 1;
+```
+- 本周
+```mysql
+SELECT * FROM 表名 WHERE YEARWEEK(DATE_FORMAT(时间字段名,'%Y-%m-%d')) = YEARWEEK(NOW());
+```
+- 上周
+```mysql
+SELECT * FROM 表名 WHERE YEARWEEK(DATE_FORMAT(时间字段名,'%Y-%m-%d')) = YEARWEEK(NOW())-1;
+```
+- 近7天
+```mysql
+SELECT * FROM 表名 WHERE DATE_SUB(CURDATE(), INTERVAL 7 DAY) <= DATE(时间字段名);
+```
+- 近30天
+```mysql
+SELECT * FROM 表名 WHERE DATE_SUB(CURDATE(), INTERVAL 30 DAY) <= DATE(时间字段名);
+```
+- 本月
+```mysql
+SELECT * FROM 表名 WHERE DATE_FORMAT(时间字段名,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m');
+```
+- 上月
+```mysql
+SELECT * FROM 表名 WHERE PERIOD_DIFF(DATE_FORMAT(NOW(),'%Y%m'),DATE_FORMAT(时间字段名,'%Y%m')) = 1;
+
+SELECT * FROM 表名 WHERE DATE_FORMAT(时间字段名,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m') ; 
+
+SELECT * FROM 表名 WHERE WEEKOFYEAR(FROM_UNIXTIME(时间字段名,'%y-%m-%d')) = WEEKOFYEAR(NOW()); 
+
+SELECT * FROM 表名 WHERE MONTH(FROM_UNIXTIME(时间字段名,'%y-%m-%d')) = MONTH(NOW()); 
+
+SELECT * FROM 表名 WHERE YEAR(FROM_UNIXTIME(时间字段名,'%y-%m-%d')) = YEAR(NOW()) AND MONTH(FROM_UNIXTIME(时间字段名,'%y-%m-%d')) = MONTH(NOW())；
+
+```
+- 近6个月
+```mysql
+SELECT * FROM 表名 WHERE 时间字段名 BETWEEN DATE_SUB(NOW(),INTERVAL 6 MONTH) AND NOW();
+```
+- 本季度
+```mysql
+SELECT * FROM 表名 WHERE QUARTER(时间字段名) = QUARTER(NOW());
+```
+- 上季度
+```mysql
+SELECT * FROM 表名 WHERE QUARTER(时间字段名) = QUARTER(DATE_SUB(NOW(),INTERVAL 1 QUARTER));
+```
+- 本年
+```mysql
+SELECT * FROM 表名 WHERE YEAR(时间字段名)=YEAR(NOW());
+```
+- 去年
+```mysql
+SELECT * FROM 表名 WHERE YEAR(时间字段名) = YEAR(DATE_SUB(NOW(),INTERVAL 1 YEAR));
+```
+
+[网络来源](https://blog.csdn.net/qq_22067469/article/details/90321979 "这个是网络测试")
+
+>注意：
+>
+> 合理利用时间函数是有效的解决方案，但有时候会适得其反，因在`时间字段`上加时间函数 会导致数据库表索引失效。
+> 以上查询语句适用于业务量较小场景。
