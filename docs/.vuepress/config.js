881c8d326a976fdcb67c56c6fb70a5b4d9618d9f
Junely <ne_july@163.com>
2020-06-28 11:52:55 +0800

    check out branch

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -40,13 +40,6 @@ module.exports = {
                 logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
                 link: 'https://vuepress-theme-reco.recoluan.com'
             },
-            // {
-            //     title: '午后南杂',
-            //     desc: 'Enjoy when you can, and endure when you must.',
-            //     email: 'recoluan@qq.com',
-            //     link: 'https://www.recoluan.com'
-            // },
-            // ...
         ],
         //在所有页面中启用自动生成侧栏
         sidebar: 'auto',
